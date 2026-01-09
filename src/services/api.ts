f8f97b81e5e0cbf8ec3ace3b025bbd7a14809bb5
unknown <andrey12233393@gmail.com>
2026-01-09 07:18:10 +0300

    feat: add auth logic

---
+++ src/services/api.ts
@@ -0,0 +1,51 @@
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+import { getToken } from './token';
+import { StatusCodes } from 'http-status-codes';
+import { processErrorHandle } from './process-error-handle';
+
+const BACKEND_URL: string = 'https://14.design.htmlacademy.pro/six-cities';
+const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true,
+};
+
+const shouldDisplayError = (response: AxiosResponse) =>
+  !!StatusCodeMapping[response.status];
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token && config.headers) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{ type: string; message: string }>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.message);
+      }
+
+      throw error;
+    }
+  );
+
+  return api;
+};
--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
 import { Offers } from '../types/offers';
 import { City } from '../types/city';
+import { AuthorizationStatus } from '../const';
+import { AuthInfo } from '../types/auth-info';
 
 export const setCity = createAction<City>('data/setCity');
 
@@ -9,3 +11,13 @@ export const setOffers = createAction<Offers>('data/setOffers');
 export const setOffersDataLoadingStatus = createAction<boolean>(
   'data/setOffersDataLoadingStatus'
 );
+
+export const requireAuthorization = createAction<AuthorizationStatus>(
+  'user/requireAuthorization'
+);
+
+export const setError = createAction<string | null>('main/setError');
+
+export const setAuthInfo = createAction<AuthInfo | null>('user/setAuthInfo');
+
+export const redirectToRoute = createAction<string>('main/redirectToRoute');
